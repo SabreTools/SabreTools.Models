68f0201c11225997e9bbb7bc0092727ae0b8f238
Matt Nadareski <mnadareski@outlook.com>
2023-11-30 19:11:52 -0500

    Add SafeDisc encrypted file entry model

---
+++ SafeDisc/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SabreTools.Models.SafeDisc
+{
+    public static class Constants
+    {
+        public const uint EncryptedFileEntrySignature1 = 0xA8726B03;
+
+        public const uint EncryptedFileEntrySignature2 = 0xEF01996C;
+    }
+}
---
+++ SafeDisc/EncryptedFileEntry.cs
@@ -0,0 +1,31 @@
+﻿namespace SabreTools.Models.SafeDisc
+{
+    /// <see href="http://blog.w4kfu.com/tag/safedisc"/>
+    public class EncryptedFileEntry
+    {
+        /// <summary>
+        /// 0xA8726B03
+        /// </summary>
+        public uint Signature1 { get; set; }
+
+        /// <summary>
+        /// 0xEF01996C
+        /// </summary>
+        public uint Signature2 { get; set; }
+
+        public uint FileNumber { get; set; }
+
+        public uint Offset1 { get; set; }
+
+        public uint Offset2 { get; set; }
+
+        public uint Unknown1 { get; set; }
+
+        public uint Unknown2 { get; set; }
+
+        /// <remarks>
+        /// 0x0D bytes
+        /// </remarks>
+        public byte[]? Name { get; set; }
+    }
+}
