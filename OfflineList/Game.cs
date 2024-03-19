4dd184583c1c74314deb8978a6390089ea3d1b34
Matt Nadareski <mnadareski@outlook.com>
2024-03-19 15:30:45 -0400

    Revert XML tag for OfflineList duplicate ID

--- OfflineList/Game.cs
+++ OfflineList/Game.cs
@@ -46,7 +46,7 @@ namespace SabreTools.Models.OfflineList
         [XmlElement("comment")]
         public string? Comment { get; set; }
 
-        [XmlElement("duplicateId")]
+        [XmlElement("duplicateID")]
         public string? DuplicateID { get; set; }
 
         #region DO NOT USE IN PRODUCTION
