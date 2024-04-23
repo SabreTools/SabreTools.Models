d844a8b5821c85c00c6b7f149aaec11786a09b6f
Matt Nadareski <mnadareski@outlook.com>
2024-04-23 16:03:20 -0400

    Add layouts for some VPK models

--- VPK/ArchiveHash.cs
+++ VPK/ArchiveHash.cs
@@ -1,17 +1,21 @@
+using System.Runtime.InteropServices;
+
 namespace SabreTools.Models.VPK
 {
     /// <see href="https://github.com/RavuAlHemio/hllib/blob/master/HLLib/VPKFile.h"/>
+    [StructLayout(LayoutKind.Sequential)]
     public sealed class ArchiveHash
     {
-        public uint ArchiveIndex { get; set; }
+        public uint ArchiveIndex;
 
-        public uint ArchiveOffset { get; set; }
+        public uint ArchiveOffset;
 
-        public uint Length { get; set; }
+        public uint Length;
 
         /// <summary>
         /// MD5
         /// </summary>
-        public byte[]? Hash { get; set; }
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x10)]
+        public byte[]? Hash;
     }
 }
