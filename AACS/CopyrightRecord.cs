108e63a0993528c726586b1e124f473f731e12d6
Matt Nadareski <mnadareski@outlook.com>
2024-04-23 13:36:24 -0400

    Add some null-terminated type flags

--- AACS/CopyrightRecord.cs
+++ AACS/CopyrightRecord.cs
@@ -1,13 +1,17 @@
+using System.Runtime.InteropServices;
+
 namespace SabreTools.Models.AACS
 {
     /// <summary>
     /// This record type is undocumented but found in real media key blocks
     /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
     public sealed class CopyrightRecord : Record
     {
         /// <summary>
         /// Null-terminated ASCII string representing the copyright
         /// </summary>
-        public string? Copyright { get; set; }
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string? Copyright;
     }
 }
--- PortableExecutable/NB10ProgramDatabase.cs
+++ PortableExecutable/NB10ProgramDatabase.cs
@@ -1,39 +1,43 @@
-﻿namespace SabreTools.Models.PortableExecutable
+﻿using System.Runtime.InteropServices;
+
+namespace SabreTools.Models.PortableExecutable
 {
     /// <summary>
     /// PDB 2.0 files
     /// </summary>
     /// <see href="https://www.debuginfo.com/articles/debuginfomatch.html"/>
+    [StructLayout(LayoutKind.Sequential)]
     public sealed class NB10ProgramDatabase
     {
         /// <summary>
         /// "CodeView signature, equal to “NB10” 
         /// </summary>
-        public uint Signature { get; set; }
+        public uint Signature;
 
         /// <summary>
         /// CodeView offset. Set to 0, because debug information
         /// is stored in a separate file.
         /// </summary>
-        public uint Offset { get; set; }
+        public uint Offset;
 
         /// <summary>
         /// The time when debug information was created (in seconds
         /// since 01.01.1970) 
         /// </summary>
-        public uint Timestamp { get; set; }
+        public uint Timestamp;
 
         /// <summary>
         /// Ever-incrementing value, which is initially set to 1 and
         /// incremented every time when a part of the PDB file is updated
         /// without rewriting the whole file. 
         /// </summary>
-        public uint Age { get; set; }
+        public uint Age;
 
         /// <summary>
         /// Null-terminated name of the PDB file. It can also contain full
         /// or partial path to the file. 
         /// </summary>
-        public string? PdbFileName { get; set; }
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string? PdbFileName;
     }
 }
