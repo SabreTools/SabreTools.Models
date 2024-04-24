b57a5455987408dca2e663070ef72d1c694680b2
Matt Nadareski <mnadareski@outlook.com>
2024-04-23 20:38:11 -0400

    Add layouts for some GCF models

--- GCF/DirectoryEntry.cs
+++ GCF/DirectoryEntry.cs
@@ -1,46 +1,45 @@
+using System.Runtime.InteropServices;
+
 namespace SabreTools.Models.GCF
 {
     /// <see href="https://github.com/RavuAlHemio/hllib/blob/master/HLLib/GCFFile.h"/>
+    [StructLayout(LayoutKind.Sequential)]
     public sealed class DirectoryEntry
     {
         /// <summary>
         /// Offset to the directory item name from the end of the directory items.
         /// </summary>
-        public uint NameOffset { get; set; }
-
-        /// <summary>
-        /// Directory item name from the end of the directory items.
-        /// </summary>
-        public string? Name { get; set; }
+        public uint NameOffset;
 
         /// <summary>
         /// Size of the item.  (If file, file size.  If folder, num items.)
         /// </summary>
-        public uint ItemSize { get; set; }
+        public uint ItemSize;
 
         /// <summary>
         /// Checksome index. (0xFFFFFFFF == None).
         /// </summary>
-        public uint ChecksumIndex { get; set; }
+        public uint ChecksumIndex;
 
         /// <summary>
         /// Flags for the directory item.  (0x00000000 == Folder).
         /// </summary>
-        public HL_GCF_FLAG DirectoryFlags { get; set; }
+        [MarshalAs(UnmanagedType.U4)]
+        public HL_GCF_FLAG DirectoryFlags;
 
         /// <summary>
         /// Index of the parent directory item.  (0xFFFFFFFF == None).
         /// </summary>
-        public uint ParentIndex { get; set; }
+        public uint ParentIndex;
 
         /// <summary>
         /// Index of the next directory item.  (0x00000000 == None).
         /// </summary>
-        public uint NextIndex { get; set; }
+        public uint NextIndex;
 
         /// <summary>
         /// Index of the first directory item.  (0x00000000 == None).
         /// </summary>
-        public uint FirstIndex { get; set; }
+        public uint FirstIndex;
     }
 }
