62bd6a4bde2110578dce4c80e164efedfe3864c2
Matt Nadareski <mnadareski@outlook.com>
2024-04-24 00:36:41 -0400

    Add a couple of constants from UnshieldSharp

--- InstallShieldCabinet/Constants.cs
+++ InstallShieldCabinet/Constants.cs
@@ -8,10 +8,14 @@ namespace SabreTools.Models.InstallShieldCabinet
 
         public const uint SignatureUInt32 = 0x28635349;
 
+        public const string HEADER_SUFFIX = "hdr";
+
+        public const string CABINET_SUFFIX = "cab";
+
         public const int COMMON_HEADER_SIZE = 20;
 
         public const int VOLUME_HEADER_SIZE_V5 = 40;
-        
+
         public const int VOLUME_HEADER_SIZE_V6 = 64;
 
         // TODO: Determine how the value "71" was chosen here
