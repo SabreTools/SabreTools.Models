2554f64277712669d463276c32d7fea104520f47
Matt Nadareski <mnadareski@outlook.com>
2024-09-26 12:15:59 -0400

    Add layout for TAR header

--- SabreTools.Models/TAR/Header.cs
+++ SabreTools.Models/TAR/Header.cs
@@ -1,101 +1,113 @@
+using System.Runtime.InteropServices;
+
 namespace SabreTools.Models.TAR
 {
     /// <see href="https://www.ibm.com/docs/en/aix/7.3?topic=files-tarh-file"/> 
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
     public sealed class Header
     {
         /// <summary>
         /// File name without a forward slash
         /// </summary>
-        /// <remarks>100 bytes</remarks>
-        public string? FileName { get; set; }
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 100)]
+        public string? FileName;
 
         /// <summary>
         /// File mode
         /// </summary>
-        /// <remarks>8 bytes, octal string representation</remarks>
-        public Mode Mode { get; set; }
+        /// <remarks>Octal string representation</remarks>
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 8)]
+        public byte[]? Mode;
 
         /// <summary>
         /// Owner's numeric user ID
         /// </summary>
-        /// <remarks>8 bytes, octal string representation</remarks>
-        public uint UID { get; set; }
+        /// <remarks>Octal string representation</remarks>
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 8)]
+        public byte[]? UID;
 
          /// <summary>
         /// Owner's numeric group ID
         /// </summary>
-        /// <remarks>8 bytes, octal string representation</remarks>
-        public uint GID { get; set; }
+        /// <remarks>Octal string representation</remarks>
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 8)]
+        public byte[]? GID;
 
         /// <summary>
         /// File size in bytes
         /// </summary>
-        /// <remarks>12 bytes, octal string representation</remarks>
-        public ulong Size { get; set; }
+        /// <remarks>Octal string representation</remarks>
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 12)]
+        public byte[]? Size;
 
         /// <summary>
         /// Last modification time in numeric Unix time format
         /// </summary>
-        /// <remarks>12 bytes, octal string representation</remarks>
-        public ulong ModifiedTime { get; set; }
+        /// <remarks>Octal string representation</remarks>
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 12)]
+        public byte[]? ModifiedTime;
 
         /// <summary>
         /// Checksum for header record
         /// </summary>
-        /// <remarks>8 bytes, octal string representation</remarks>
-        public ushort Checksum { get; set; }
+        /// <remarks>Octal string representation</remarks>
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 8)]
+        public byte[]? Checksum;
 
         /// <summary>
         /// Link indicator (file type) / Type flag
         /// </summary>
-        public TypeFlag TypeFlag { get; set; }
+        public TypeFlag TypeFlag;
 
         /// <summary>
         /// Linked path name or file name
         /// </summary>
-        /// <remarks>100 bytes</remarks>
-        public string? LinkName { get; set; }
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 100)]
+        public string? LinkName;
 
         /// <summary>
         /// UStar indicator, "ustar", then NUL
         /// </summary>
-        /// <remarks>6 bytes</remarks>
-        public string? Magic { get; set; }
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 6)]
+        public string? Magic;
 
         /// <summary>
         /// UStar version, "00"
         /// </summary>
-        /// <remarks>3 bytes</remarks>
-        public string? Version { get; set; }
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 3)]
+        public string? Version;
 
         /// <summary>
         /// Owner user name
         /// </summary>
-        /// <remarks>32 bytes</remarks>
-        public string? UserName { get; set; }
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string? UserName;
 
         /// <summary>
         /// Owner group name
         /// </summary>
-        /// <remarks>32 bytes</remarks>
-        public string? GroupName { get; set; }
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string? GroupName;
 
         /// <summary>
         /// Device major number
         /// </summary>
-        /// <remarks>8 bytes, octal(?)</remarks>
-        public string? DevMajor { get; set; }
+        /// <remarks>Octal string representation(?)</remarks>
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 8)]
+        public string? DevMajor;
 
         /// <summary>
         /// Device minor number
         /// </summary>
-        /// <remarks>8 bytes, octal(?)</remarks>
-        public string? DevMinor { get; set; }
+        /// <remarks>Octal string representation(?)</remarks>
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 8)]
+        public string? DevMinor;
 
         /// <summary>
         /// Path name without trailing slashes
         /// </summary>
         /// <remarks>155 bytes</remarks>
-        public string? Prefix { get; set; }
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 155)]
+        public string? Prefix;
     }
 }
