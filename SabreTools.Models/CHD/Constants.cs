8725ec0337cdd26fbddf07e61432d5a98ed967ea
Matt Nadareski <mnadareski@outlook.com>
2024-10-19 23:58:56 -0400

    Port more CHD info from SabreTools

---
+++ SabreTools.Models/CHD/Constants.cs
@@ -0,0 +1,17 @@
+namespace SabreTools.Models.CHD
+{
+    public static class Constants
+    {
+        public const string SignatureString = "MComprHD";
+
+        #region Header Sizes
+
+        public const int HeaderV1Size = 76;
+        public const int HeaderV2Size = 80;
+        public const int HeaderV3Size = 120;
+        public const int HeaderV4Size = 108;
+        public const int HeaderV5Size = 124;
+
+        #endregion
+    }
+}
