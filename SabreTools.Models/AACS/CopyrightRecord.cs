362abfdc6491b2da373956f4f40135a6271f9f87
Matt Nadareski <mnadareski@outlook.com>
2024-11-27 10:38:58 -0500

    Remove layout from CopyrightRecord

--- SabreTools.Models/AACS/CopyrightRecord.cs
+++ SabreTools.Models/AACS/CopyrightRecord.cs
@@ -5,7 +5,6 @@ namespace SabreTools.Models.AACS
     /// <summary>
     /// This record type is undocumented but found in real media key blocks
     /// </summary>
-    [StructLayout(LayoutKind.Sequential)]
     public sealed class CopyrightRecord : Record
     {
         /// <summary>
