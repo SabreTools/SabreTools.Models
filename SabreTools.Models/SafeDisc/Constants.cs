4c76ce123037d543cdf362ff7a53ead7d5d0be2c
Matt Nadareski <mnadareski@outlook.com>
2025-07-21 08:10:25 -0400

    Add SecuROM DFA models
    All research thanks to HeroponRikiBestest

--- SabreTools.Models/SafeDisc/Constants.cs
+++ SabreTools.Models/SafeDisc/Constants.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SabreTools.Models.SafeDisc
+﻿namespace SabreTools.Models.SafeDisc
 {
     public static class Constants
     {
