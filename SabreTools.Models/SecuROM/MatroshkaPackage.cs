6547242f93059c318a1f1e7228b14d68c5ecc271
Matt Nadareski <mnadareski@outlook.com>
2025-07-21 11:27:20 -0400

    Clean up more Matroskha comments

--- SabreTools.Models/SecuROM/MatroshkaPackage.cs
+++ SabreTools.Models/SecuROM/MatroshkaPackage.cs
@@ -1,16 +1,18 @@
 namespace SabreTools.Models.SecuROM
 {
     /// <summary>
-    /// Securom Matroschka Package, a package contained in an executable as
-    /// a section. Offered by SecuROM, its main purpose seems to be managing
-    /// some sort of SecuROM-related operation involving multiple temporary
-    /// files contained within the package. Observed in Release Control
-    /// executables, Product Activation Revocation executables, and in some
-    /// regular Product-Activation-protected releases (such as the digital
-    /// download releases of Neverwinter Nights 2 and Test Drive Unlimited)
-    /// where the game executable, paul.dll and other PA-related files are
-    /// stored in the matroschka package. 
+    /// Securom Matroschka Package PE section
     /// </summary>
+    /// <remarks>
+    /// Offered by SecuROM, its main purpose seems to be managing some sort
+    /// of SecuROM-related operation involving multiple temporary files
+    /// contained within the package. Observed in Release Control executables,
+    /// Product Activation Revocation executables, and in some regular
+    /// Product-Activation-protected releases (such as the digital download
+    /// releases of Neverwinter Nights 2 and Test Drive Unlimited) where the
+    /// game executable, paul.dll and other PA-related files are stored in
+    /// the matroschka package.
+    /// </remarks>
     public class MatroshkaPackage
     {
         /// <summary>
