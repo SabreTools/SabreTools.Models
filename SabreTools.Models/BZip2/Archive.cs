2350c1af276395055d10a8e0a6a9ccd78bf6274d
Matt Nadareski <mnadareski@outlook.com>
2025-08-28 07:46:23 -0400

    Add placeholder models

---
+++ SabreTools.Models/BZip2/Archive.cs
@@ -0,0 +1,10 @@
+namespace SabreTools.Models.BZip2
+{
+    /// <summary>
+    /// This is a placeholder model for future work
+    /// </summary>
+    public class Archive
+    {
+
+    }
+}
---
+++ SabreTools.Models/SevenZip/Archive.cs
@@ -0,0 +1,10 @@
+namespace SabreTools.Models.SevenZip
+{
+    /// <summary>
+    /// This is a placeholder model for future work
+    /// </summary>
+    public class Archive
+    {
+
+    }
+}
