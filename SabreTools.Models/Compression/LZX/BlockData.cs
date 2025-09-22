973a94b3408bb4457600ef1b67df51c3eadc495f
Matt Nadareski <mnadareski@outlook.com>
2025-09-22 10:33:33 -0400

    Move LZX models to IO

--- SabreTools.Models/Compression/LZX/BlockData.cs
+++
@@ -1,8 +0,0 @@
-namespace SabreTools.Models.Compression.LZX
-{
-    /// <see href="https://interoperability.blob.core.windows.net/files/MS-PATCH/%5bMS-PATCH%5d.pdf"/>
-    public abstract class BlockData
-    {
-        // No common fields between all block data
-    }
-}
