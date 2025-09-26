f101df48aea565182051bfe741aa69c71e36a67c
Matt Nadareski <mnadareski@outlook.com>
2025-09-26 11:39:29 -0400

    Migrate support file models to Serialization

--- SabreTools.Models/DVD/Constants.cs
+++
@@ -1,9 +0,0 @@
-namespace SabreTools.Models.DVD
-{
-    public static class Constants
-    {
-        public const string VideoManagerIFOSignature = "DVDVIDEO-VMG";
-
-        public const string VideoTitleSetIFOSignature = "DVDVIDEO-VTS";
-    }
-}
--- SabreTools.Models/RequiredAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace SabreTools.Models
-{
-    /// <summary>
-    /// Marks a property as required on write
-    /// </summary>
-    /// <remarks>TODO: Use reflection to determine required fields on write</remarks>
-    [System.AttributeUsage(System.AttributeTargets.Property)]
-    public class RequiredAttribute : System.Attribute { }
-}
